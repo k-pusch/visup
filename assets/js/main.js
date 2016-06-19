ad1232168a77c5996d0f4e8e497b9fe0d91cb268
Kristina Pusch <krissi.pusch@gmail.com>
2016-06-19 10:34:36 +0200

    improved currency formatting

--- assets/js/main.js
+++ assets/js/main.js
@@ -106,7 +106,33 @@
         },
 
         formatCurrency: function (value) {
-            return value.toFixed(2).replace('.', ',')
+            var fixed = value.toFixed(2),
+                bits = fixed.split('.'),
+                number = bits[0],
+                decimal = bits[1];
+
+            if (number.length <= 3) {
+                return number + ',' + decimal;
+            }
+
+            var remainder = number.length % 3,
+                head, tail, groups;
+
+            if (remainder) {
+                head = number.substr(0, remainder);
+                tail = number.substr(remainder);
+                groups = [head];
+
+            } else {
+                tail = number;
+                groups = [];
+            }
+
+            for (var start = 0; start < tail.length; start += 3) {
+                groups.push(tail.substr(start, 3));
+            }
+
+            return groups.join('.') + ',' + decimal;
         },
 
         shorten: function (value, length) {
